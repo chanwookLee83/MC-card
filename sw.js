2c77df6f6f37cfa4fefe06a9fec4f655f5a57ae9
chanwookLee83 <cw2@kakao.com>
2026-06-06 00:41:01 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // ⚙️ 머신카드 Service Worker v5.2
-const CACHE_NAME = 'machine-card-v30';
-const STATIC_CACHE = 'machine-card-static-v30';
+const CACHE_NAME = 'machine-card-v31';
+const STATIC_CACHE = 'machine-card-static-v31';
 
 // 캐시할 파일 목록
 const CACHE_FILES = [
